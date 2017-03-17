eebd02fb5cc14f2566d02b14afb487488842cd92
Pete Hawkins <pete@phawk.co.uk>
2017-03-17 20:38:09 +0000

    Tidy up error codes

--- config/initializers/error_handling.rb
+++ config/initializers/error_handling.rb
@@ -8,7 +8,7 @@ module Sinatra
     module Helpers
       def halt_with_400_bad_request(message = nil)
         message ||= "Bad request"
-        halt 400, json({ message: message })
+        halt 400, json({ error_code: "bad_request", message: message })
       end
 
       def halt_with_401_authorization_required(message = nil, realm = "App Name")
@@ -18,17 +18,17 @@ module Sinatra
         # Warden steps on 401s, set this message so wardens failure
         # app will use it and proxy it on to the end user
         request.env['warden.api.error'] = message
-        halt 401, json({ message: message })
+        halt 401, json({ error_code: "unauthorized", message: message })
       end
 
       def halt_with_403_forbidden(message = nil)
         message ||= "Forbidden"
-        halt 403, json({ message: message })
+        halt 403, json({ error_code: "forbidden", message: message })
       end
 
       def halt_with_404_not_found(message = nil)
         message ||= "Resource not found"
-        halt 404, json({ message: message })
+        halt 404, json({ error_code: "not_found", message: message })
       end
 
       def halt_with_422_unprocessible_entity
@@ -52,6 +52,7 @@ module Sinatra
           }
         end
         halt 422, json({
+          error_code: "validation_failed",
           message: "Validation failed",
           errors: errors
         })
@@ -59,6 +60,7 @@ module Sinatra
 
       def halt_with_500_internal_server_error
         halt 500, json({
+          error_code: "internal_error",
           message: "Internal server error: this is a problem on our end and we've been notified of the issue"
         })
       end
@@ -72,14 +74,10 @@ module Sinatra
       end
 
       app.error Sinatra::NotFound do
-        status 404
-        json({ error: "not_found", message: "Endpoint '#{request.path_info}' not found" })
+        halt_with_404_not_found("Endpoint '#{request.path_info}' not found")
       end
 
       app.error do
-        # if ::Exceptional::Config.should_send_to_api?
-        #   ::Exceptional::Remote.error(::Exceptional::ExceptionData.new(env['sinatra.error']))
-        # end
         # err_name = env['sinatra.error'].name
         halt_with_500_internal_server_error
       end
