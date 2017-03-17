e485b02c99657845ab9e42889c8db306657f0678
Colin Young <hello@colin-young.co.uk>
2017-03-17 16:47:39 +0000

    testing user registrations

--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -2,6 +2,23 @@ require "spec_helper"
 
 describe "Api::v1::UsersStory", type: :api do
   describe "POST /v1/users" do
+    before do
+      authenticate_client
+    end
 
+    it "has validation" do
+      post "/v1/users", { name: "Batman" }
+
+      expect(http_status).to eq(400)
+      expect(response_json[:data][:message]).to match(/Validation failed/)
+      expect(response_json[:data][:errors]).not_to be_nil
+    end
+
+    it "creates a user and returns an api token" do
+      post "/v1/users", { name: "Batman", email: "batman@robin.com", password: "supersecretpassword" }
+
+      puts response_json
+      expect(http_status).to eq(200)
+    end
   end
 end
