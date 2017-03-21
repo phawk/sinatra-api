31e6f9d561378fe8011e14ee6e1e07824384e1ae
Pete Hawkins <pete@phawk.co.uk>
2017-03-21 21:53:02 +0000

    use strings for response_json as jsonapi keys will be dasherized and symbols dont work with dashes

--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -10,19 +10,19 @@ describe "Api::v1::UsersStory", type: :api do
       post "/v1/users", { name: "Batman" }
 
       expect(http_status).to eq(422)
-      expect(response_json[:error_code]).to eq("validation_failed")
-      expect(response_json[:errors]).not_to be_nil
+      expect(response_json["error_code"]).to eq("validation_failed")
+      expect(response_json["errors"]).not_to be_nil
     end
 
     it "creates a user" do
       post "/v1/users", { name: "Batman", email: "batman@robin.com", password: "supersecretpassword" }
 
       expect(http_status).to eq(200)
-      expect(response_json[:data][:attributes][:name]).to eq("Batman")
-      expect(response_json[:data][:attributes][:email]).to eq("batman@robin.com")
+      expect(json_attrs["name"]).to eq("Batman")
+      expect(json_attrs["email"]).to eq("batman@robin.com")
 
       # Actually created the user
-      expect(User.find(response_json[:data][:id]).email).to eq("batman@robin.com")
+      expect(User.find(response_json["data"]["id"]).email).to eq("batman@robin.com")
     end
   end
 end
