c52449a5be50d446de4f8faf87ca64c4b5c19cd0
Pete Hawkins <pete@phawk.co.uk>
2017-03-31 21:05:03 +0100

    Update logger based on environment

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,7 +1,14 @@
 require 'logger'
 
-# Log to a file
-# $logger = Logger.new(File.expand_path(File.join(__dir__, "..", "..", "log", ENV.fetch('RACK_ENV', 'development')+".log")))
+$logger = begin
+  case ENV['RACK_ENV']
+  when "test"
+    Logger.new("/dev/null")
+  else
+    # Log to a file
+    # Logger.new(File.expand_path(File.join(__dir__, "..", "..", "log", ENV.fetch('RACK_ENV', 'development')+".log")))
 
-# Log to STDOUT
-$logger = Logger.new(STDOUT)
+    # Log to STDOUT
+    Logger.new(STDOUT)
+  end
+end
