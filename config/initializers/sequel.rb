4ee754aa6c5a6164c7ab333a3fc43e0d5911e1d5
Pete Hawkins <pete@phawk.co.uk>
2017-04-01 15:28:26 +0100

    Add max conns to sequel configuration

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,4 +1,4 @@
-DB = Sequel.connect(ENV["RACK_ENV"] == "test" ? ENV["TEST_DATABASE_URL"] : ENV["DATABASE_URL"])
+DB = Sequel.connect(ENV["RACK_ENV"] == "test" ? ENV["TEST_DATABASE_URL"] : ENV["DATABASE_URL"], max_connections: Integer(ENV['MAX_THREADS']) + 1)
 
 Sequel::Model.plugin :timestamps, update_on_create: true
 Sequel::Model.plugin :validation_helpers
