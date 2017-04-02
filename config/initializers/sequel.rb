d07214e28cfbf84bbc8b6c8a1b5f12277399b17e
Pete Hawkins <pete@phawk.co.uk>
2017-04-02 11:18:09 +0100

    Fix nil max threads issue

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,4 +1,4 @@
-DB = Sequel.connect(ENV["RACK_ENV"] == "test" ? ENV["TEST_DATABASE_URL"] : ENV["DATABASE_URL"], max_connections: Integer(ENV['MAX_THREADS']) + 1)
+DB = Sequel.connect(ENV["RACK_ENV"] == "test" ? ENV["TEST_DATABASE_URL"] : ENV["DATABASE_URL"], max_connections: Integer(ENV.fetch('MAX_THREADS', 5)) + 1)
 DB.extension :pg_array, :pg_json, :pagination
 
 Sequel::Model.plugin :timestamps, update_on_create: true
