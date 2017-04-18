692733bea3998dbe3f639ed6cce53f624181b85f
Pete Hawkins <pete@phawk.co.uk>
2017-04-18 11:08:27 +0100

    Add NullLogger as it may be useful in tests

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,9 +1,10 @@
 require 'logger'
+require 'null_logger'
 
 $logger = begin
   case ENV['RACK_ENV']
   when "test"
-    Logger.new("/dev/null")
+    NullLogger.new
   else
     # Log to a file
     # Logger.new(File.expand_path(File.join(__dir__, "..", "..", "log", ENV.fetch('RACK_ENV', 'development')+".log")))
