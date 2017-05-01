defdf60c03191ac010f8a2e6b6e454cba287fc08
Pete Hawkins <pete@phawk.co.uk>
2017-05-01 17:16:29 +0100

    Add sortable_by_column sequel plugin

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -5,3 +5,4 @@ Sequel::Model.plugin :timestamps, update_on_create: true
 Sequel::Model.plugin :validation_helpers
 Sequel::Model.plugin :basic_pg_search
 Sequel::Model.plugin :first_or_initialize
+Sequel::Model.plugin :sortable_by_column
