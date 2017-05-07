8843c793937fb7e22206cd4ae69d4a03b255bd84
Pete Hawkins <pete@phawk.co.uk>
2017-05-07 10:55:41 +0100

    Log sequel queries

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,5 +1,7 @@
 DB = Sequel.connect(ENV["RACK_ENV"] == "test" ? ENV["TEST_DATABASE_URL"] : ENV["DATABASE_URL"], max_connections: Integer(ENV.fetch('MAX_THREADS', 5)) + 1)
 DB.extension :pg_array, :pg_json, :pagination
+DB.loggers << $logger
+DB.sql_log_level = :debug
 
 Sequel::Model.plugin :timestamps, update_on_create: true
 Sequel::Model.plugin :validation_helpers
