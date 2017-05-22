eb8a77629f7b14572de664eb9d772c9880b0ca28
Pete Hawkins <pete@phawk.co.uk>
2017-05-22 23:17:50 +0100

    rubocop: dont suppress exceptions

--- lib/password_verifier.rb
+++ lib/password_verifier.rb
@@ -4,6 +4,7 @@ class PasswordVerifier
   def initialize(digest)
     @bcrypt = BCrypt::Password.new(digest)
   rescue BCrypt::Errors::InvalidHash
+    @bcrypt = nil
   end
 
   def verify(plaintext)
