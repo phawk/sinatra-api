a9b85849829b784248f94adf1940be8575bec287
Pete Hawkins <pete@phawk.co.uk>
2017-06-10 12:58:35 +0100

    Remove uneeded auth helpers

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -5,24 +5,10 @@ module Api
         env["warden"]
       end
 
-      def ensure_client!
-        warden.authenticate! :access_token, :client_id
-
-        # If we use client_id auth we need to set the current client manually
-        @oauth_client = env["warden.oauth_client"] if env["warden.oauth_client"]
-      end
-
-      def ensure_client_secret!
-        warden.authenticate! :client_secret
-
-        # If we use client_secret auth we need to set the current client manually
-        @oauth_client = env["warden.oauth_client"] if env["warden.oauth_client"]
-      end
-
       # Soft authenticate - sets warden.user if strategy passes
       #                   - does nothing if strategy fails
-      def authenticate(strategy: :access_token)
-        warden.authenticate(strategy)
+      def authenticate
+        warden.authenticate(:jwt)
       end
 
       # Hard authenticate - sets warden.user if strategy passes
@@ -31,16 +17,8 @@ module Api
         warden.authenticate!
       end
 
-      def current_token
-        @current_token ||= warden.user
-      end
-
       def current_user
-        @current_user ||= current_token&.user
-      end
-
-      def current_client
-        @oauth_client ||= current_token&.client_application
+        @current_user ||= warden.user
       end
     end
   end
