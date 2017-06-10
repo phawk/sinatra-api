108bdb1175927efc85d4e1aac745b53c8e23ad7c
Pete Hawkins <pete@phawk.co.uk>
2017-06-10 16:07:43 +0100

    Add pending specs for all the emails for user-auth

--- spec/jobs/user_auth_email_job_spec.rb
+++ spec/jobs/user_auth_email_job_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 RSpec.describe UserAuthEmailJob do
-  #
+  context "user signups" do
+    it "sends an email"
+  end
+
+  context "password resets" do
+    it "sends an email"
+  end
+
+  context "password updates" do
+    it "sends an email"
+  end
 end
