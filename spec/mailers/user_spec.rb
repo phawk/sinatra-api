5d1811db774c5f2b62901caf47910f95655afb75
Pete Hawkins <pete@phawk.co.uk>
2017-06-11 15:54:55 +0100

    Test all 3 user-auth emails

--- spec/mailers/user_spec.rb
+++ spec/mailers/user_spec.rb
@@ -12,14 +12,47 @@ RSpec.describe Api::Mailers::User do
 
       expect(last_email.to.first).to eq("test@example.org")
       expect(last_email.subject).to eq("Welcome aboard!")
-      expect(last_email.body.parts.first.to_s).to include("Welcome")
-      expect(last_email.body.parts.first.to_s).to include("Tester")
+
+      body = last_email.body.parts.first.to_s
+      expect(body).to include("Welcome")
+      expect(body).to include("Tester")
     end
   end
 
   describe "#password_reset" do
+    it "sends a password reset email" do
+      subject.password_reset(
+        to: "test@example.org",
+        user: {
+          name: "Tester"
+        },
+        reset_token: "abc123"
+      )
+
+      expect(last_email.to.first).to eq("test@example.org")
+      expect(last_email.subject).to eq("Password reset instructions")
+
+      body = last_email.body.parts.first.to_s
+      expect(body).to include("reset")
+      expect(body).to include("Tester")
+      expect(body).to include("abc123")
+    end
   end
 
   describe "#password_updated" do
+    it "sends a password updated email" do
+      subject.password_updated(
+        to: "test@example.org",
+        user: {
+          name: "Tester"
+        }
+      )
+
+      expect(last_email.to.first).to eq("test@example.org")
+      expect(last_email.subject).to eq("Your password has been changed")
+
+      body = last_email.body.parts.first.to_s
+      expect(body).to include("Tester")
+    end
   end
 end
