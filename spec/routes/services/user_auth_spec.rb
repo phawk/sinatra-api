4ea7b4267254f298f102e5b9bad5881b91470421
Pete Hawkins <pete@phawk.co.uk>
2017-06-11 15:25:12 +0100

    Write an integration spec for the user-auth service

--- spec/routes/services/user_auth_spec.rb
+++ spec/routes/services/user_auth_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe "User Auth Service", type: :api do
     get "/auth"
     expect(http_status).to eq(200)
   end
+
+  it "provides tokens" do
+    post "/auth/signup", email: "test@example.org", password: "mcflurries", info: { name: "Tester" }
+    expect(http_status).to eq(201)
+    expect(response_json["token_type"]).to eq("Bearer")
+    expect(response_json["refresh_token"]).not_to be_nil
+
+    payload = SigninToken.new.parse(response_json["token"])
+    expect(payload["exp"]).to be_within(10).of(Time.now.to_i + 3600)
+    expect(payload["email"]).to eq("test@example.org")
+    expect(payload["user_id"]).to be_a(Numeric)
+    expect(payload["name"]).to eq("Tester")
+  end
 end
